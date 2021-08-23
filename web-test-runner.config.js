59c244ab852e09d842f42234e79a6ae27736db03
bailinca <8807461+bailinca@users.noreply.github.com>
2021-08-23 14:28:52 +0300

    tests

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable unicorn/prefer-module */
 process.env.NODE_ENV = "test";
 
 module.exports = {
