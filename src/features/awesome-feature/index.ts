971f9191872eb4fbc317774f862add17fa818895
bailinca <8807461+bailinca@users.noreply.github.com>
2021-08-24 19:22:53 +0300

    comm

---
+++ src/features/awesome-feature/index.ts
@@ -0,0 +1,5 @@
+// Think of a feature as a library or a module that is self-contained but can expose different parts to other features via its entry point.
+// the index.ts file behaves as the public API of the feature.
+
+// Example usage:
+// import { AwesomeComponent } from "src/features/awesome-feature"
