8f412d6711d3ddd0b3935bd5c68db0fd8e077a44
bailinca <8807461+bailinca@users.noreply.github.com>
2021-08-25 23:03:02 +0300

    store

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import App from "./App";
+import { store } from "src/services/store";
 import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.querySelector("#root")
 );
