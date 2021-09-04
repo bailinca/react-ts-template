3f572f257dad7465e8479aca0bb21da6ff2bcf1f
bailinca <8807461+bailinca@users.noreply.github.com>
2021-09-04 18:01:05 +0300

    upd test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,15 @@
 import { render } from "@testing-library/react";
 import { expect } from "chai";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 
 describe("<App>", () => {
   it("renders learn react link", () => {
-    const { getByText } = render(<App />);
+    const { getByText } = render(
+      <Router>
+        <App />
+      </Router>
+    );
     const linkElement = getByText(/learn react/i);
     expect(document.body.contains(linkElement));
   });
