d9e41e0a408338214a077b359bb55771cccb58e8
bailinca <8807461+bailinca@users.noreply.github.com>
2021-09-08 17:57:08 +0300

    dfg

--- src/features/counter/counterHooks.ts
+++ src/features/counter/counterHooks.ts
@@ -1,11 +1,11 @@
 import useSWR from "swr";
 
 export function useAmount() {
-  const { data, error } = useSWR(`/api/counter/amount`);
+  const { data, error, isValidating } = useSWR(`/api/counter/amount`);
 
   return {
     amount: data?.amount ?? 1,
-    loading: !error && !data, // use isValidating ??
+    loading: isValidating,
     error,
   };
 }
